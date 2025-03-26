2134316b6bb6f803a77b1608247cc2a5ecb27075
yll0rd <leoyoumbi83@gmail.com>
2025-03-26 20:13:31 +0100

    feat: implement UpdateTaskStatusDto for task status updates in Tasks module

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,9 +9,10 @@ import {
   Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './tasks.model';
+import { Task } from './tasks.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -49,8 +50,9 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
   ): Task | null {
+    const { status } = updateTaskStatusDto;
     console.log(id, status);
     return this.tasksService.updateTaskStatus(id, status);
   }
